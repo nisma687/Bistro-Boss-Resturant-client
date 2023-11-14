dcd3c3a1e3475db4c61330d7c101a4cc7de008da
Nisma <20-41982-1@student.aiub.edu>
2023-11-15 03:02:54 +0600

    first commit

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import image1 from "../../../../src/assets/home/01.jpg";
+import image2 from "../../../../src/assets/home/02.jpg";
+import image3 from "../../../../src/assets/home/03.png";
+// import image4 from "../../../../src/assets/home/04.jpg";
+// import image5 from "../../../../src/assets/home/05.jpg";
+// import image6 from "../../../../src/assets/home/06.jpg";
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <div>
+                    <img src={image1} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={image2} />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={image3} />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is Home Page</h2>
+            <Banner />
+        </div>
+    );
+};
+
+export default Home;
